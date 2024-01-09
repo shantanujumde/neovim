8568424108d27407310aa5a07e873f9cbf0bfcd6
shantanujumde <jumde21@gmail.com>
2024-01-09 19:38:43 +0530

    init neovim

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,19 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.erb_lint,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+      },
+    })
+
+    vim.lsp.buf.format({ timeout_ms = 2000 })
+    vim.keymap.set("n", "<C-s>", vim.lsp.buf.format, {})
+  end,
+}
